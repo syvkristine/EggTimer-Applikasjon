b3596e7c975c1aa85cde8fae6649579b8fc6c542
Olivier Klaver <o.klaver@gmail.com>
2017-05-18 21:24:06 +0200

    Add semicolons
    https://www.youtube.com/watch?v=M94ii6MVilw

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 
-var path = require('path')
-var url = require('url')
+var path = require('path');
+var url = require('url');
 
 
 // Global reference of the window object
-var win
+var win;
+
 
 function createWindow () {
   // Create the browser window.
@@ -19,7 +20,7 @@ function createWindow () {
     pathname: path.join(__dirname, 'www/index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools.
   //win.webContents.openDevTools()
@@ -30,30 +31,30 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null
-  })
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function(){
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    app.quit();
+  };
+});
 
 app.on('activate', function(){
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
