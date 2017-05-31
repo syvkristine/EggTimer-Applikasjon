7770d1b1c2a8c9b4cc3bd7ecc3c2730a2c873065
Sofie Staal <minasofiestaallillelien@Minas-MacBook-Pro.local>
2017-05-31 16:25:14 +0200

    skulle oppdatert mer underveis

--- main.js
+++ main.js
@@ -11,27 +11,31 @@ var url = require('url');
 var win;
 
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 965,
+        height: 800,
+        resizable: false
+    })
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'www/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'www/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  // Open the DevTools.
-  //win.webContents.openDevTools()
+    // Open the DevTools.
+    //win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', function(){
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  });
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    });
 }
 
 // This method will be called when Electron has finished
@@ -40,23 +44,21 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function(){
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  };
+app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    };
 });
 
-app.on('activate', function(){
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
---
+++ www/js/result.js
@@ -0,0 +1,6 @@
+food;
+
+var match = Number(sessionStorage.getItem('product'));
+var product = food[match];
+
+console.log(product);
