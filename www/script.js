9502a9fc8213cbd91b34e143a999b2f9988d103e
kristine syverstad fossum <93kristine@live.no>
2018-05-25 13:05:01 +0200

    add soft, medium and hard funksjons, and startbt funksjon

---
+++ www/script.js
@@ -0,0 +1,52 @@
+
+var startbt = document.querySelector('.startBt');
+
+
+var softEgg = document.querySelector('.softEgg');
+var mediumEgg = document.querySelector('.mediumEgg');
+var hardEgg = document.querySelector('.hardEgg');
+
+
+softEgg.addEventListener('click', handleSoftEggClick)
+
+
+function handleSoftEggClick (event) {
+
+    console.log ('sotf egg er clicket på');
+    //legg til html 
+
+}
+
+
+mediumEgg.addEventListener('click', handleMediumEggClick)
+
+
+function handleMediumEggClick (event) {
+
+    console.log ('medium egg er cliket på');
+    //legg til html 
+
+}
+
+
+hardEgg.addEventListener('click', handleHardEggClick)
+
+
+function handleHardEggClick (event) {
+
+    console.log ('hard egg er cliket på');
+    //legg til html 
+
+}
+
+
+startbt.addEventListener('click', handleStartClick)
+
+
+function handleStartClick (event) {
+
+    console.log ('start klokken');
+    //legg til html 
+
+}
+
